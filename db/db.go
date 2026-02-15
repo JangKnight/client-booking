bfdb703eb9bbd43309ec273f75f2b46dc463b8c0
Professor Knight <profdevent@gmail.com>
2026-02-14 20:30:40 -0500

    add sql lite setups

---
+++ db/db.go
@@ -0,0 +1,53 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	DB, err := sql.Open("sqlite3", "api.db")
+
+	if err != nil {
+		panic("Failed to connect to database.")
+	}
+
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+}
+
+func createTables() {
+	createUserTable := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		username TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL
+	);
+	`
+	createBookingTable := `
+	CREATE TABLE IF NOT EXISTS bookings (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		customer_email TEXT NOT NULL,
+		customer_name TEXT NOT NULL,
+		service TEXT NOT NULL,
+		location TEXT NOT NULL,
+		date_time DATETIME NOT NULL,
+		user_id INTEGER NOT NULL,
+		FOREIGN KEY (user_id) REFERENCES users(id)
+	);
+	`
+
+	_, err := DB.Exec(createUserTable)
+	if err != nil {
+		panic("Failed to create users table.")
+	}
+
+	_, err = DB.Exec(createBookingTable)
+	if err != nil {
+		panic("Failed to create bookings table.")
+	}
+}
